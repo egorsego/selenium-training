7e930dc67c77cd7578fbcf518b3e6bddfb4d8e5b
egorsego <egorsego@git.training>
2018-06-19 23:29:22 +0300

    Basic project structure

---
+++ selenium_course/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace selenium_course
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
